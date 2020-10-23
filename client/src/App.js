835a81108aedfca4876291aee96966450d8a309d
Lanny Rivest <lanny.rivest@gmail.com>
2020-10-23 18:56:50 -0400

    Connects to Database

--- client/src/App.js
+++ client/src/App.js
@@ -8,28 +8,7 @@ import { SET_USERS } from './reducers/dataReducer';
 
 
 function App() {
-
-  const { state, dispatch } = useApplicationData();
-
-  useEffect(() => {
-
-    axios({
-      method: 'GET',
-      url: '/api/users'
-    })
-      .then(result => dispatch({ type: SET_USERS, users: result.data }))
-      .catch(err => console.log(err.message))
-
-  }, [])
-  const userList = state.users.map((user) => (<li key={user.id}> {user.first_name} {user.last_name} {user.email}</li>));
-
-
-  return (<div className="App" >
-    <h1> Users </h1>
-
-    <ul> {userList} </ul>
-  </div >
-  );
+    return <div>Sup Racoons!</div>
 }
 
 export default App;
