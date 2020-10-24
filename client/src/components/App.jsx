4640755b582f70325bdea065a0b800ab8fe8ed42
Fatima Nguyen <fatima3455@hotmail.com>
2020-10-24 16:30:47 -0400

    add weather-ring component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,11 +3,20 @@ import axios from 'axios';
 // import useApplicationData from '../hooks/useApplicationData';
 // import { SET_USERS } from '../reducers/dataReducer';
 
-
+import WeatherRing from './WeatherRing'
 
 
 function App() {
-    return <div>Sup Racoons!</div>
+return (
+    <main>
+        <WeatherRing
+            mainWeather='Sunny'
+            feelsLikeTemp='23'
+            minTemp='18'
+            maxTemp='29'
+        />
+    </main>
+);
 }
 
 export default App;
--- client/src/components/WeatherRing.jsx
+++ client/src/components/WeatherRing.jsx
@@ -1,2 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+
+export default function WeatherRing(props) {
+  return (
+    <article className='weather-ring'>
+      <h2>{props.mainWeather}</h2>
+      <h3>Feels like {props.feelsLikeTemp}°</h3>
+      <h4>{props.minTemp}° / {props.maxTemp}°</h4>
+    </article>
+  );
+};
