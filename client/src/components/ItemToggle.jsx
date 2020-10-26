3fac398feed3e8af176d30501e624448285f174f
Fatima Nguyen <fatima3455@hotmail.com>
2020-10-26 08:27:36 -0400

    fix toggle down icon

--- client/src/components/ItemToggle.jsx
+++ client/src/components/ItemToggle.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCaretDown} from '@fortawesome/free-solid-svg-icons'
 
 export default function ItemToggle(props) {
-  return <button onClick={props.toggle}><i className="fas fa-caret-down"></i></button>;
+  return <button onClick={props.toggle}> <FontAwesomeIcon icon={faCaretDown}/> </button>;
 };
