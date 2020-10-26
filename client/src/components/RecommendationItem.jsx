6ab1e253636f4596e9ea5e36c4caecdc5144bd92
Fatima Nguyen <fatima3455@hotmail.com>
2020-10-26 08:14:22 -0400

    remove recommendation item check state

--- client/src/components/RecommendationItem.jsx
+++ client/src/components/RecommendationItem.jsx
@@ -2,20 +2,24 @@ import React, { useState } from 'react';
 import ItemToggle from './ItemToggle'
 
 export default function RecommendationItem(props) {
+  const checked = props.type === 'done'? true : false; //ensures that items in done list show as checked and others show as unchecked
   const [toggle, setToggle] = useState(false);
-  const [checked, setCheck] = useState(false);
+  // const [checked, setCheck] = useState(typeCondition);
 
   const onClick = () => {
-    setCheck(!checked);
-    if (!checked || (checked && props.type === 'done')) {
       props.handleCheck(props.id, props.type);
-    }
   };
+  // const onClick = () => {
+  //   setCheck(!checked);
+  //   if (!checked || (checked && props.type === 'done')) {
+  //     props.handleCheck(props.id, props.type);
+  //   }
+  // };
   return (
     <li>
       <div>
         <div onClick={onClick}>
-          <input type='checkbox' checked={checked} onChange={() => setCheck(prev => prev)}/> {props.name}
+          <input type='checkbox' checked={checked} /*onChange={() => setCheck(prev => prev)}*//> {props.name}
         </div>
         <div>
           <ItemToggle toggle = {() => setToggle(!toggle)}/>
