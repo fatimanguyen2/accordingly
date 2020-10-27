f51ddf290c3c3c6cea280faa9d30cc11d0ac2649
Lanny Rivest <lanny.rivest@gmail.com>
2020-10-27 17:40:06 -0400

    Add main weather call

--- express_backend/APIs/open_weather.js
+++ express_backend/APIs/open_weather.js
@@ -4,13 +4,26 @@ const exclusions = 'minutely'
 
 
 const getWeather = (location) => {
-  return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${location.x}&lon=${location.y}&exclude=${exclusions}&appid=${key}`)
+  return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${location.x}&lon=${location.y}&exclude=${exclusions}&appid=${key}&units=metric`)
     .then(response => response.data)
     .catch(err => err);
   }
 
+const getMainWeather = (location) => {
+  return getWeather(location)
+  .then(data => {
+    console.log(data.hourly)
+      return {
+        mainWeather: data.current.weather.map(condtion => condtion.main),
+        feelsLikeTemp: data.current.feels_like,
+        actualTemp: data.current.temp,
+        feels_likeMin : Math.min(...data.hourly.filter((hour, index) => index < 8).map(hour => hour.feels_like)),
+        feels_likeMax : Math.max(...data.hourly.filter((hour, index) => index < 8).map(hour => hour.feels_like))
+      }
+    })
+}
 
 
 module.exports = {
-  getWeather
+  getMainWeather,
 };
