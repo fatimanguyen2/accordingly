da39f75356b00ce258bf039a5648a2c7cf1c0415
Fatima Nguyen <fatima3455@hotmail.com>
2020-10-27 13:16:50 -0400

    commit before pulling from main

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -14,11 +14,12 @@ export const Settings = props => {
         <div>
           <h1>Settings</h1>
           <form autoComplete='off' onSubmit={event => event.preventDefault()}>
-            <label><FontAwesomeIcon icon={faMapMarkerAlt}/>Home:</label><br/>
+            <label for='address'><FontAwesomeIcon icon={faMapMarkerAlt}/>Home:</label>
+            <input id='address' value={address} onChange={event => setAddress(event.target.value)}/><br/>
             <label for='startOfDay'><FontAwesomeIcon icon={faClock}/>Start of Day:</label>
-            <input id='startOfDay' value={startOfDay}/>
+            <input id='startOfDay' value={startOfDay} onChange={event => setStartOfDay(event.target.value)} type='time'/>
           </form>
-          {/* <Button>Save</Button> */}
+          {/* <Button onClick={}>Save</Button> */}
 
         </div> :
         <div><h1>Please register/log in.</h1></div>
