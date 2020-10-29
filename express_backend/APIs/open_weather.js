d81e7fd47d74bf2e3e30efb875e43c5e9605128f
Fatima Nguyen <fatima3455@hotmail.com>
2020-10-28 22:38:11 -0400

    add test6pm to backend

--- express_backend/APIs/open_weather.js
+++ express_backend/APIs/open_weather.js
@@ -42,13 +42,13 @@ const getDetailedForcast = (schedule) => {
   return Promise.all(allForecasts)
 }
 
-
+const test6AM = moment("2020-10-28T06");
 ///need refactorging call only one time
 const getForecastCategory = (event) => {
   const start = event.start_time;
   const in48h = moment().add(48, 'h');
   if (moment(start).isBefore(in48h)) {
-    const hour = moment(start).diff(moment(), "hour")
+    const hour = moment(start).diff(test6AM, "hour")
     if (hour > 0) {
       return getWeather(event.destination)
         .then(data => data.hourly[hour].weather[0].main)
