9c8ead2c3465c60b135d210e3e5fe897adb057e7
Lanny Rivest <lanny.rivest@gmail.com>
2020-10-29 12:29:29 -0400

    complete verbose address in schem and seends

--- express_backend/db/schema/02_entries.sql
+++ express_backend/db/schema/02_entries.sql
@@ -7,5 +7,8 @@ CREATE TABLE entries(
   created_at TIMESTAMP DEFAULT now(),
   destination POINT NOT NULL,
   is_active BOOLEAN DEFAULT TRUE NOT NULL,
+  address VARCHAR(255),
+  city VARCHAR(255),
+  postal_code VARCHAR(255),
   user_id INTEGER REFERENCES entries(id) ON DELETE CASCADE
 );
--- express_backend/db/seeds/02_entries.sql
+++ express_backend/db/seeds/02_entries.sql
@@ -1,14 +1,14 @@
 INSERT INTO
-  entries(title, is_outdoor, destination, user_id)
+  entries(title, is_outdoor, destination, address, city, postal_code, user_id)
 VALUES
-('commute', TRUE, point(49.23010, -123.10867), 2),
-('morning run', TRUE, point(49.259432, -123.100795), 2),
-('skatepark', TRUE, point(43.70564, -79.42154), 1),
-('d&d nights', FALSE, point(43.67398, -79.42789), 3),
-('Payday', FALSE, point(43.67559, -79.23953), 4),
-('Going to see dad', FALSE, point(43.67969, -79.29683), 3),
-('Gymn-a-thon', TRUE, point(43.66808, -79.30635), 5),
-('Fundraiser', FALSE, point(43.67969, -79.29683), 4),
-('Skate competition', TRUE, point(43.66858, -79.35459), 1),
-('Comicon', FALSE, point(44.22624, -76.49317), 3),
-('Boring convention', FALSE, point(49.38654, -123.13254), 2);
+('commute', TRUE, point(49.23010, -123.10867), '6078 Manitoba St', 'Vancouver', 'V5Y 3T4', 2),
+('morning run', TRUE, point(49.259432, -123.100795), '2846 Main St', 'Vancouver', 'V5T 3G2', 2),
+('skatepark', TRUE, point(43.70564, -79.42154), '613 Roselawn Ave', 'Toronto', 'M5N 1K7', 1),
+('d&d nights', FALSE, point(43.67398, -79.42789), '1223 Shaw St', 'Toronto', 'M6G 3N7', 3),
+('Payday', FALSE, point(43.67740, -79.24621), '97 Crescentwood Rd', 'Toronto', 'M1N 1E4', 4),
+('Going to see dad', FALSE, point(43.67969, -79.29683), '12 Walter St', 'Toronto', 'M4E 2Z1', 3),
+('Gymn-a-thon', TRUE, point(43.66808, -79.30635), '187 Boardwalk Dr', 'Toronto', 'M4L 3X9', 5),
+('Fundraiser', FALSE, point(43.67969, -79.29683), '12 Walter St', 'Toronto', 'M4E 2Z1', 4),
+('Skate competition', TRUE, point(43.66858, -79.35459), '453 Broadview Ave', 'Toronto', 'M4K 2N3', 1),
+('Comicon', FALSE, point(44.22624, -76.49317), '62 5th Field Company Lane', 'Toronto', 'K7L', 3),
+('Boring convention', FALSE, point(49.38754, -123.14254), '1567 Townline Rd', 'Abbotsford', 'V2T 6E1', 2);
