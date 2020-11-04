3355c6f0409d36c1b0dc9b14af666ea969f061b9
Fatima Nguyen <fatima3455@hotmail.com>
2020-11-04 14:51:32 -0500

    add logo ring in about page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,6 +4,7 @@ import './About.scss';
 export const About = () => {
   return (
     <div className='about'>
+      <div className='logo-ring'><h1>Accordingly</h1></div>
       <h1>About Us</h1>
       <p>Site made by...</p>
       <h1 className='about__team-name'>The Loaded Raccoons!</h1>
